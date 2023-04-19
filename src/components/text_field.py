3d173c1aa88839e84a97a9c955e4deeb90836f11
Caramajau <caramia0505@gmail.com>
2023-04-19 19:50:33 +0200

    put the path to save in a variable

--- src/components/text_field.py
+++ src/components/text_field.py
@@ -3,20 +3,22 @@ import tkinter as tk
 class TextField(tk.Text):
     def __init__(self, height: int = 700, width: int = 350) -> None:
         super().__init__(height = height, width = width)
+        self.path = "save\\saved_text.txt"
     
     """
     Function for saving input from the text field. 
     """
     def save_input(self) -> None:
         text_input = self.get("1.0", "end-1c")
-        with open("save\\saved_text.txt","w+") as file:
+
+        with open(self.path, "w+") as file:
             file.write(text_input)
 
     """
     Function for loading the input
     """
     def load_saved_input(self) -> None:
-        with open("save\\saved_text.txt", "r+") as file:
+        with open(self.path, "r+") as file:
             text = file.read()
 
             self.insert("1.0", text)
