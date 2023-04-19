7c057d87e99dac1779e0d66fa48d7340dddb9de1
Caramajau <caramia0505@gmail.com>
2023-04-19 20:03:10 +0200

    moved the create of the save folder to app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 from components.window import Window
+import os
 
 if __name__ == "__main__":
+    if (os.path.exists("save") == False):
+        os.makedirs("save")
+
     the_window = Window()
     the_window.mainloop()
--- src/components/text_field.py
+++ src/components/text_field.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import os
 
 class TextField(tk.Text):
     def __init__(self, height: int = 700, width: int = 350) -> None:
@@ -12,9 +11,6 @@ class TextField(tk.Text):
     def save_input(self) -> None:
         text_input = self.get("1.0", "end-1c")
 
-        if (os.path.exists("save") == False):
-            os.makedirs("save")
-
         with open(self.path, "w+") as file:
             file.write(text_input)
 
@@ -22,8 +18,6 @@ class TextField(tk.Text):
     Function for loading the input
     """
     def load_saved_input(self) -> None:
-        if (os.path.exists("save") == False):
-            os.makedirs("save")
 
         with open(self.path, "r+") as file:
             text = file.read()
