24c5e2737502147239c9125f75c1fe0456ecf593
Caramajau <caramia0505@gmail.com>
2023-05-02 19:15:32 +0200

    Made it so that the taskbar icon matches window

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 from components.window import Window
 import os
+import ctypes
 
 if __name__ == "__main__":
     if (os.path.exists("save") == False):
         os.makedirs("save")
+
+    # this changes so that the taskbar matches the window icon.
+    myappid = "mycompany.myproduct.subproduct.version" # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
     the_window = Window()
     the_window.mainloop()
