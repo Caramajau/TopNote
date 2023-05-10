6d529dc06ecaa59bf1247fac9f83d66e91c9d7d3
Caramajau <caramia0505@gmail.com>
2023-05-10 16:20:16 +0200

    Changed string and checks Windows for taskbar icon

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
 from components.window import Window
 import os
 import ctypes
+import platform
 
 if __name__ == "__main__":
     if (os.path.exists("save") == False):
         os.makedirs("save")
 
-    # this changes so that the taskbar matches the window icon.
-    my_app_id = "mycompany.myproduct.subproduct.version" # arbitrary string
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
+    # From what I understand this is only necessary on Windows.
+    if (platform.system()) == "Windows":
+        # this changes so that the taskbar matches the window icon.
+        my_app_id = "TopNote"
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
 
     the_window = Window()
     the_window.mainloop()
