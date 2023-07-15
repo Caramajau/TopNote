45b4cd29c8cdabbf32fc4a501ecec2fd8ebe2f40
Caramajau <caramia0505@gmail.com>
2023-07-15 14:21:30 +0200

    Added return types

--- src/components/window.py
+++ src/components/window.py
@@ -28,7 +28,7 @@ class Window(tk.Tk):
         self.set_tooltip_for_buttons()
 
 
-    def put_items_on_grid(self):
+    def put_items_on_grid(self) -> None:
         self.save_button.grid(row = 0, column = 0, sticky = tk.NW)
         self.load_button.grid(row = 0, column = 1, sticky = tk.NW)
         self.clear_button.grid(row = 0, column = 2, sticky = tk.NW)
@@ -37,27 +37,27 @@ class Window(tk.Tk):
         self.text_field.grid(row = 1)
 
 
-    def bind_shortcuts(self):
+    def bind_shortcuts(self) -> None:
         self.bind("<Control-s>", lambda event: self.text_field.save_input())
         self.bind("<Control-l>", lambda event: self.text_field.load_saved_input())
         self.bind("<Alt-c>", lambda event: self.text_field.clear_text())
     
 
-    def set_tooltip_for_buttons(self):
+    def set_tooltip_for_buttons(self) -> None:
         self.create_tooltips()
         self.raise_tooltips()
 
-    def create_tooltips(self):
+    def create_tooltips(self) -> None:
         self.tooltip_for_save_button = ToolTip(self.save_button, "Saves text - CTRL+S")
         self.tooltip_for_load_button = ToolTip(self.load_button, "Loads text - CTRL+L")
         self.tooltip_for_clear_button = ToolTip(self.clear_button, "Clears text - ALT+C")
 
-    def raise_tooltips(self):
+    def raise_tooltips(self) -> None:
         self.tooltip_for_save_button.wm_attributes("-topmost", 1)
         self.tooltip_for_load_button.wm_attributes("-topmost", 1)
         self.tooltip_for_clear_button.wm_attributes("-topmost", 1)
 
-def resource_path(relative_path):
+def resource_path(relative_path) -> str:
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
