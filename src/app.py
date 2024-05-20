a28c22c49f3ed96b38c1e71fd94e3807d75e0b6d
Caramajau <caramia0505@gmail.com>
2024-05-20 15:47:04 +0200

    Extracted method in app

--- src/app.py
+++ src/app.py
@@ -1,18 +1,20 @@
 from components.window import Window
-import os
 import ctypes
 import platform
 import tkinter as tk
 
 def main():
+    __configure_task_bar_icon()
+
+    the_window: tk.Tk = Window()
+    the_window.mainloop()
+
+def __configure_task_bar_icon() -> None:
     # From what I understand this is only necessary on Windows.
     if platform.system() == "Windows":
         # this changes so that the taskbar matches the window icon.
         my_app_id: str = "TopNote"
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
-
-    the_window: tk.Tk = Window()
-    the_window.mainloop()
 
 if __name__ == "__main__":
     main()
