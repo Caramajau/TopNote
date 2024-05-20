9ee4c5210b5f4085f8ff7a08e37901ae959ef3be
Caramajau <caramia0505@gmail.com>
2024-05-20 14:21:36 +0200

    did some formatting in app

--- src/app.py
+++ src/app.py
@@ -2,16 +2,20 @@ from components.window import Window
 import os
 import ctypes
 import platform
+import tkinter as tk
 
-if (__name__ == "__main__"):
-    if (os.path.exists("save") == False):
+def main():
+    if not os.path.exists("save"):
         os.makedirs("save")
 
     # From what I understand this is only necessary on Windows.
-    if (platform.system() == "Windows"):
+    if platform.system() == "Windows":
         # this changes so that the taskbar matches the window icon.
-        my_app_id:str = "TopNote"
+        my_app_id: str = "TopNote"
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
 
-    the_window = Window()
+    the_window: tk.Tk = Window()
     the_window.mainloop()
+
+if __name__ == "__main__":
+    main()
