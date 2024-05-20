8a7e499f06e9300d74ba1eda132b0685ddb69e7a
Caramajau <caramia0505@gmail.com>
2024-05-20 15:40:47 +0200

    Moved comments into methods

--- src/components/text_field.py
+++ src/components/text_field.py
@@ -9,25 +9,25 @@ class TextField(tk.Text):
         self.FILE_PATH: str = FilePaths.SAVE_PATH.value
         self.file_handler: FileHandler = FileHandler(self.FILE_PATH)
     
-    """
-    Saves input from the text field. 
-    """
     def save_input(self) -> None:
+        """
+        Saves input from the text field. 
+        """
         text_input: str = self.get("1.0", "end-1c")
         self.file_handler.write(text_input)
 
-    """
-    Loads saved text and inserts it to the text field.
-    """
     def load_saved_input(self) -> None:
+        """
+        Loads saved text and inserts it to the text field.
+        """
         try:
             text: str = self.file_handler.read()
             self.insert("1.0", text)
         except FileNotFoundError as e:
             print(f"Exception occurred: {e} \nFile at {self.FILE_PATH} has to be saved before loading")
 
-    """
-    Clears the text field.
-    """
     def clear_text(self) -> None:
+        """
+        Clears the text field.
+        """
         self.delete("1.0","end")
