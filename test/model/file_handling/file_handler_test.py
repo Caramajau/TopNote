7e2b44725564718625278d24389480b5429bf53f
Caramajau <caramia0505@gmail.com>
2024-05-22 16:53:23 +0200

    renamed constant in test

--- test/model/file_handling/file_handler_test.py
+++ test/model/file_handling/file_handler_test.py
@@ -8,26 +8,26 @@ from src.model.file_handling.file_paths import FilePaths
 
 class TestFileHandler(ut.TestCase):
     FILE_PATH: str = os.path.join("data", "test_file.txt")
-    TEST_TEST: str = "This is a test text"
-    ANOTHER_TEST_TEST: str = "This is another test text"
+    TEST_STRING: str = "This is a test text"
+    ANOTHER_TEST_STRING: str = "This is another test text"
 
     def setUp(self) -> None:
         self.file_handler: FileHandler = FileHandler(self.FILE_PATH)
         
     def test_write_given_text_creates_file(self) -> None:
-        self.file_handler.write(self.TEST_TEST)
+        self.file_handler.write(self.TEST_STRING)
         self.assertTrue(os.path.exists(self.FILE_PATH))
 
     def test_write_given_text_reads_same_text(self) -> None:
-        self.file_handler.write(self.TEST_TEST)
+        self.file_handler.write(self.TEST_STRING)
         text: str = self.file_handler.read()
-        self.assertEqual(text, self.TEST_TEST)
+        self.assertEqual(text, self.TEST_STRING)
         
     def test_write_given_text_reads_same_text_after_overwriting(self) -> None:
-        self.file_handler.write(self.TEST_TEST)
-        self.file_handler.write(self.ANOTHER_TEST_TEST)
+        self.file_handler.write(self.TEST_STRING)
+        self.file_handler.write(self.ANOTHER_TEST_STRING)
         text: str = self.file_handler.read()
-        self.assertEqual(text, self.ANOTHER_TEST_TEST)
+        self.assertEqual(text, self.ANOTHER_TEST_STRING)
 
     def test_read_given_file_does_not_exist_returns_empty_string(self) -> None:
         text: str = self.file_handler.read()
