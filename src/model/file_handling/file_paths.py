df9ce2b9b97173b8c066c16b848da863715d4852
Caramajau <caramia0505@gmail.com>
2024-05-25 13:06:39 +0200

    Added documentation for file paths

--- src/model/file_handling/file_paths.py
+++ src/model/file_handling/file_paths.py
@@ -3,5 +3,6 @@ import os
 
 @unique
 class FilePaths(Enum):
+    """Enum class for file paths."""
     SAVE_PATH = os.path.join("data", "saved_text.txt")
     ICON_PATH = os.path.join("resources", "t_icon.png")
