4f6501fb6c4253793272268fd46ed0913a861b63
Caramajau <caramia0505@gmail.com>
2025-05-25 16:05:35 +0200

    Format file handling

--- model/file_handling/file_paths.py
+++ model/file_handling/file_paths.py
@@ -1,9 +1,10 @@
 from enum import Enum, unique
 import os
 
+
 @unique
 class FilePaths(Enum):
     """Enum class for file paths."""
-    
+
     SAVE_PATH = os.path.join("data", "saved_text.txt")
     ICON_PATH = os.path.join("resources", "t_icon.png")
