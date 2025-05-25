5e3f05fed6ad6c7afd3a21993c70d936f1f57a1b
Caramajau <caramia0505@gmail.com>
2025-05-25 16:08:23 +0200

    Format app

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import tkinter as tk
 
 from view.window import Window
 
+
 def main() -> None:
     __configure_taskbar_icon()
 
     the_window: tk.Tk = Window()
     the_window.mainloop()
+
 
 def __configure_taskbar_icon() -> None:
     # From what I understand this is only necessary on Windows.
@@ -16,6 +18,7 @@ def __configure_taskbar_icon() -> None:
         # this changes so that the taskbar matches the window icon.
         my_app_id: str = "TopNote"
         ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
+
 
 if __name__ == "__main__":
     main()
